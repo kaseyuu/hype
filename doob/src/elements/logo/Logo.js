60ba52fdbe5c54e0a6306fffaf8dfb9a14498f3e
Kasey <kaseywu130@gmail.com>
2024-02-27 14:49:26 +1100

    update logo
    update logo in mobile view

--- doob/src/elements/logo/Logo.js
+++ doob/src/elements/logo/Logo.js
@@ -1,13 +1,13 @@
 import PropTypes from "prop-types";
 import React from 'react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Logo = ({image, image2}) => {
-    return(
+const Logo = ({ image, image2 }) => {
+    return (
         <div className="logo">
             <Link to={process.env.PUBLIC_URL + "/"}>
-                <img className="logo-light" src={process.env.PUBLIC_URL + image} alt="Corporate Logo" />
-                <img className="logo-dark" src={process.env.PUBLIC_URL + image2} alt="Corporate Logo" />
+                <img className="logo-light" src="./hype/hype1.png" alt="Corporate Logo" />
+                <img className="logo-dark" src="./hype/hype1.png" alt="Corporate Logo" />
             </Link>
         </div>
     )
